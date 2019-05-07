dcffa5dcf4ac0023214183ab6b245cdacd3db7d8
zhangxunice <2462832901@qq.com>
2019-05-07 11:20:27 +0800

    完成我的发布

--- pages/user/myIssue.js
+++ pages/user/myIssue.js
@@ -5,74 +5,171 @@ Page({
    * 页面的初始数据
    */
   data: {
-    index:0
+    index: 0,
+    isPopping: false, //是否已经弹出
+    animPlus: {}, //旋转动画
+    animCollect: {}, //item位移,透明度
+    animTranspond: {}, //item位移,透明度
+    animInput: {}, //item位移,透明度
+
 
   },
   changeTabbar(e) {
     this.setData({
       index: e.currentTarget.dataset.id
     })
-  }, 
-  bindchange: function (e) {
+  },
+  bindchange: function(e) {
     const that = this;
     that.setData({
       currentData: e.detail.current
     })
   },
 
+  //点击弹出
+  plus: function() {
+    if (this.data.isPopping) {
+      //缩回动画
+      this.popp();
+      this.setData({
+        isPopping: false
+      })
+    } else if (!this.data.isPopping) {
+      //弹出动画
+      this.takeback();
+      this.setData({
+        isPopping: true
+      })
+    }
+  },
+  //弹出动画
+  popp: function() {
+    //plus顺时针旋转
+    var animationPlus = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationcollect = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationTranspond = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationInput = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    animationPlus.rotateZ(180).step();
+    animationcollect.translate(-60, -60).rotateZ(0).opacity(1).step();
+    animationTranspond.translate(-100, 0).rotateZ(0).opacity(1).step();
+    animationInput.translate(-60, 60).rotateZ(0).opacity(1).step();
+    this.setData({
+      animPlus: animationPlus.export(),
+      animCollect: animationcollect.export(),
+      animTranspond: animationTranspond.export(),
+      animInput: animationInput.export(),
+    })
+  },
+  //收回动画
+  takeback: function() {
+    //plus逆时针旋转
+    var animationPlus = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationcollect = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationTranspond = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    var animationInput = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-out'
+    })
+    animationPlus.rotateZ(0).step();
+    animationcollect.translate(0, 0).rotateZ(0).opacity(0).step();
+    animationTranspond.translate(0, 0).rotateZ(0).opacity(0).step();
+    animationInput.translate(0, 0).rotateZ(0).opacity(0).step();
+    this.setData({
+      animPlus: animationPlus.export(),
+      animCollect: animationcollect.export(),
+      animTranspond: animationTranspond.export(),
+      animInput: animationInput.export(),
+    })
+  },
+  input: function() {
+    console.log("input")
+  },
+  transpond: function() {
+    wx.navigateTo({
+      url: '/pages/community/posting/posting',
+    })
+  },
+  collect: function() {
+    console.log("collect")
+  },
+
+
+
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
